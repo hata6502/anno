fded0ef14d7949493a5f1465388f51dc367c519e
Tomoyuki Hata <7702653+hata6502@users.noreply.github.com>
2023-04-28 20:51:14 +0900

    verticalAlign = text-bottom

--- src/content.ts
+++ src/content.ts
@@ -206,6 +206,7 @@ chrome.runtime.onMessage.addListener(async (contentMessage: ContentMessage) => {
 
               iframeElement.style.all = "revert";
               iframeElement.style.border = "none";
+              iframeElement.style.verticalAlign = "text-bottom";
               iframeElement.style.width = `${size}px`;
               iframeElement.style.height = `${size}px`;
 
