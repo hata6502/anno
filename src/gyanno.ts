0965df6fd2b7b75c07e760ca5148df0734ed8168
Tomoyuki Hata <7702653+hata6502@users.noreply.github.com>
2023-09-17 21:43:02 +0900

    Line Break

--- src/gyanno.ts
+++ src/gyanno.ts
@@ -1,20 +1,10 @@
-// Gyazo Teamsとかでは動かないかも。
-// どこのページ使おうか。
-//   画像ページ?
-//   print?
-//   Chrome Extension独自?
-// 縦書きのとき、l to rになっているため複数行の選択が難しい。
-// アイコンなしモード?
-// ガタガタの黄色annotation修正
-//   vertical-align: top;  横方向のときだけ。
-//   color: transparent
-//   もっと薄く
-
 // @ts-expect-error
 import eaw from "eastasianwidth";
 
 interface Annotation {
   description: string;
+  paddingCount: number;
+  breakCount: number;
   minX: number;
   minY: number;
   maxX: number;
@@ -53,16 +43,23 @@ styleElement.textContent = `
   }
 
   .gyanno {
-    position: absolute;
-    color: red;
-    font-family: monospace;
-    font-size: 10px;
-    transform-origin: top left;
-    user-select: text;
-    white-space: nowrap;
+    &.overlay {
+      position: absolute;
+      color: transparent;
+      font-family: monospace;
+      font-size: 10px;
+      transform-origin: top left;
+      user-select: text;
+      white-space: pre;
 
-    &::selection {
-      background: rgb(0, 0, 255, 0.125);
+      &::selection {
+        background: rgb(0, 0, 255, 0.125);
+      }
+    }
+
+    &.break {
+      position: absolute;
+      visibility: hidden;
     }
   }
 `;
@@ -79,75 +76,10 @@ const gyanno = async () => {
     return;
   }
 
-  const imageViewerElement = document.querySelector(
-    ".image-box-component .image-viewer"
-  );
-  if (!(imageViewerElement instanceof HTMLElement)) {
-    return;
-  }
-  const imageViewerRect = imageViewerElement.getBoundingClientRect();
-  if (!imageViewerRect.width || !imageViewerRect.height) {
-    return;
-  }
-
-  const getStyle = ({ description, minX, minY, maxX, maxY }: Annotation) => {
-    let width = maxX - minX;
-    let height = maxY - minY;
-    if (description.length === 1) {
-      width = height = Math.max(width, height);
-    }
-
-    return {
-      left: minX,
-      top: minY,
-      width,
-      height,
-      size: Math.min(width, height),
-    };
-  };
-
-  const getNeighborAnnotation = (a: Annotation, b: Annotation) => {
-    const aStyle = getStyle(a);
-    const bStyle = getStyle(b);
-
-    const getIsIntersected = (margin: number) =>
-      aStyle.left + aStyle.width + aStyle.size * margin >=
-        bStyle.left - bStyle.size * margin &&
-      aStyle.top + aStyle.height + aStyle.size * margin >=
-        bStyle.top - bStyle.size * margin &&
-      bStyle.left + bStyle.width + bStyle.size * margin >=
-        aStyle.left - aStyle.size * margin &&
-      bStyle.top + bStyle.height + bStyle.size * margin >=
-        aStyle.top - aStyle.size * margin;
-
-    if (!getIsIntersected(0.5)) {
-      return;
-    }
-
-    const padding = getIsIntersected(0.25) ? "" : " ";
-    const neighbor = {
-      description: `${a.description}${padding}${b.description}`,
-      minX: Math.min(a.minX, b.minX),
-      minY: Math.min(a.minY, b.minY),
-      maxX: Math.max(a.maxX, b.maxX),
-      maxY: Math.max(a.maxY, b.maxY),
-    };
-
-    const neighborStyle = getStyle(neighbor);
-    if (
-      Math.abs(neighborStyle.size - aStyle.size) >= aStyle.size * 0.5 ||
-      Math.abs(neighborStyle.size - bStyle.size) >= bStyle.size * 0.5
-    ) {
-      return;
-    }
-
-    return neighbor;
-  };
-
   cleanUp?.();
   cleanUp = undefined;
 
-  const match = location.pathname.match(/^\/([0-9a-z]{32})/);
+  const match = location.pathname.match(/^\/([0-9a-z]{32})$/);
   if (!match) {
     return;
   }
@@ -160,37 +92,78 @@ const gyanno = async () => {
 
       const annotations: Annotation[] = (metadata.ocrAnnotations ?? []).map(
         // @ts-expect-error
-        ({ description, boundingPoly }) => ({
-          description,
-          minX: boundingPoly.vertices[0].x,
-          minY: boundingPoly.vertices[0].y,
-          maxX: boundingPoly.vertices[2].x,
-          maxY: boundingPoly.vertices[2].y,
-        })
-      );
-      console.time("merge");
+        ({ description, boundingPoly }) => {
+          // @ts-expect-error
+          const xs = boundingPoly.vertices.map(({ x }) => x);
+          // @ts-expect-error
+          const ys = boundingPoly.vertices.map(({ y }) => y);
 
-      let mergedAnnotations;
+          return {
+            description,
+            paddingCount: 0,
+            breakCount: 0,
+            minX: Math.min(...xs),
+            minY: Math.min(...ys),
+            maxX: Math.max(...xs),
+            maxY: Math.max(...ys),
+          };
+        }
+      );
+
+      let mergedAnnotation;
       do {
-        mergedAnnotations = undefined;
+        mergedAnnotation = undefined;
         for (const [aIndex, a] of annotations.slice(0, -1).entries()) {
           const bIndex = aIndex + 1;
           const b = annotations[bIndex];
 
-          mergedAnnotations = getNeighborAnnotation(a, b);
-          if (mergedAnnotations) {
-            annotations[aIndex] = mergedAnnotations;
+          mergedAnnotation = getNeighborAnnotation(a, b);
+          if (mergedAnnotation) {
+            annotations[aIndex] = mergedAnnotation;
             annotations.splice(bIndex, 1);
             break;
           }
         }
-      } while (mergedAnnotations);
-      console.log(annotations.length);
+      } while (mergedAnnotation);
+
+      for (const [aIndex, a] of annotations.slice(0, -1).entries()) {
+        const b = annotations[aIndex + 1];
+
+        const aStyle = getStyle(a);
+        const bStyle = getStyle(b);
+
+        const breakCount = Math.round(
+          Math.abs(
+            (aStyle.isHorizontal
+              ? bStyle.top - aStyle.top
+              : aStyle.left + aStyle.width - (bStyle.left + bStyle.width)) /
+              aStyle.size
+          )
+        );
+        a.breakCount = breakCount;
+
+        if (!breakCount) {
+          a.paddingCount = Math.round(
+            Math.abs(
+              (aStyle.isHorizontal
+                ? bStyle.left - (aStyle.left + aStyle.width)
+                : bStyle.top - (aStyle.top + aStyle.height)) / aStyle.size
+            )
+          );
+        }
+      }
+
       return { annotations, scale };
     })();
   cache.set(url, annotationsPromise);
   const { annotations, scale } = await annotationsPromise;
 
+  const imageViewerElement = document.querySelector(
+    ".image-box-component .image-viewer"
+  );
+  if (!(imageViewerElement instanceof HTMLElement)) {
+    return;
+  }
   // 実験なので、いったん拡大機能を止める。
   imageViewerElement.style.cursor = "unset";
   imageViewerElement.style.pointerEvents = "none";
@@ -198,7 +171,11 @@ const gyanno = async () => {
     ".image-box-component .image-close-btn-bg"
   ).style.display = "none";
 
-  const divElements = annotations.map((annotation) => {
+  const imageViewerRect = imageViewerElement.getBoundingClientRect();
+  if (!imageViewerRect.width || !imageViewerRect.height) {
+    return;
+  }
+  const overlayElements = annotations.map((annotation) => {
     const style = getStyle(annotation);
 
     const boxWidth = (style.width / scale.width) * imageViewerRect.width;
@@ -210,40 +187,105 @@ const gyanno = async () => {
     const textScale = fontSize / 10;
     const textLength = eaw.length(annotation.description) * 0.5 * fontSize;
 
-    const divElement = document.createElement("div");
+    const overlayElement = document.createElement("div");
 
-    divElement.textContent = annotation.description;
-    divElement.classList.add("gyanno");
+    overlayElement.textContent = `${annotation.description}${" ".repeat(
+      annotation.paddingCount
+    )}`;
+    overlayElement.classList.add("gyanno", "overlay");
 
-    divElement.style.left = `${
+    overlayElement.style.left = `${
       (style.left / scale.width) * imageViewerRect.width +
       imageViewerRect.left +
       scrollX
     }px`;
-    divElement.style.top = `${
+    overlayElement.style.top = `${
       (style.top / scale.height) * imageViewerRect.height +
       imageViewerRect.top +
       scrollY
     }px`;
 
-    divElement.style.letterSpacing = `${
+    overlayElement.style.letterSpacing = `${
       (boxLength - textLength) / annotation.description.length / textScale
     }px`;
-    divElement.style.transform = `scale(${textScale})`;
-    divElement.style.writingMode =
-      style.width >= style.height ? "horizontal-tb" : "vertical-rl";
+    overlayElement.style.transform = `scale(${textScale})`;
+    overlayElement.style.writingMode = style.isHorizontal
+      ? "horizontal-tb"
+      : "vertical-rl";
 
-    return divElement;
+    const breakElement = document.createElement("span");
+    breakElement.innerHTML = "<br />".repeat(annotation.breakCount);
+    breakElement.classList.add("gyanno", "break");
+    overlayElement.append(breakElement);
+
+    return overlayElement;
   });
 
-  for (const divElement of divElements) {
-    document.body.append(divElement);
+  for (const overlayElement of overlayElements) {
+    document.body.append(overlayElement);
   }
   cleanUp = () => {
-    for (const divElement of divElements) {
-      divElement.remove();
+    for (const overlayElement of overlayElements) {
+      overlayElement.remove();
     }
   };
+};
+
+const getStyle = ({ description, minX, minY, maxX, maxY }: Annotation) => {
+  let width = maxX - minX;
+  let height = maxY - minY;
+  if (description.length === 1) {
+    width = height = Math.max(width, height);
+  }
+
+  return {
+    left: minX,
+    top: minY,
+    width,
+    height,
+    isHorizontal: width >= height,
+    size: Math.min(width, height),
+  };
+};
+
+const getNeighborAnnotation = (a: Annotation, b: Annotation) => {
+  const aStyle = getStyle(a);
+  const bStyle = getStyle(b);
+
+  const getIsIntersected = (margin: number) =>
+    aStyle.left + aStyle.width + aStyle.size * margin >=
+      bStyle.left - bStyle.size * margin &&
+    aStyle.top + aStyle.height + aStyle.size * margin >=
+      bStyle.top - bStyle.size * margin &&
+    bStyle.left + bStyle.width + bStyle.size * margin >=
+      aStyle.left - aStyle.size * margin &&
+    bStyle.top + bStyle.height + bStyle.size * margin >=
+      aStyle.top - aStyle.size * margin;
+
+  if (!getIsIntersected(1)) {
+    return;
+  }
+
+  const padding = getIsIntersected(0.5) ? "" : " ";
+  const neighbor = {
+    description: `${a.description}${padding}${b.description}`,
+    paddingCount: 0,
+    breakCount: 0,
+    minX: Math.min(a.minX, b.minX),
+    minY: Math.min(a.minY, b.minY),
+    maxX: Math.max(a.maxX, b.maxX),
+    maxY: Math.max(a.maxY, b.maxY),
+  };
+
+  const neighborStyle = getStyle(neighbor);
+  if (
+    Math.abs(neighborStyle.size - aStyle.size) >= aStyle.size * 0.5 ||
+    Math.abs(neighborStyle.size - bStyle.size) >= bStyle.size * 0.5
+  ) {
+    return;
+  }
+
+  return neighbor;
 };
 
 const handle = async () => {
