de3aeafca0971088534b73b50e4468227357d5c5
Tomoyuki Hata <7702653+hata6502@users.noreply.github.com>
2023-11-12 01:55:16 +0900

    staticRange

--- src/textQuoteInjection.tsx
+++ src/textQuoteInjection.tsx
@@ -9,18 +9,19 @@ export interface TextQuoteInjectionConfig {
   id: string;
   textQuoteSelector: TextQuoteSelector;
   whiteout: string;
-  inject: (range: Range) => State;
+  inject: (range: Range) => Pick<State, "range" | "cleanUp">;
 }
 
 interface Injection {
   config: TextQuoteInjectionConfig;
-  minDistance: number;
   states: State[];
 }
 
 interface State {
   range: Range;
   cleanUp: () => void;
+  staticRange: StaticRange;
+  distance: number;
 }
 
 let injections: Injection[] = [];
@@ -40,7 +41,6 @@ export const injectByTextQuote = (configs: TextQuoteInjectionConfig[]) => {
     (config) =>
       injections.find((injection) => injection.config.id === config.id) ?? {
         config,
-        minDistance: Infinity,
         states: [],
       }
   );
@@ -51,28 +51,53 @@ export const injectByTextQuote = (configs: TextQuoteInjectionConfig[]) => {
 const handle = () => {
   mutationObserver.disconnect();
   try {
-    const textIndex = getTextIndex(document.body);
-    injections = injections
-      .map((injection) => ({
-        injection,
-        ...getNearestRanges(
-          textIndex,
-          injection.config.textQuoteSelector,
-          injection.minDistance
-        ),
-      }))
-      .map(({ injection, nearestRanges, minDistance }) => ({
+    let textIndex = getTextIndex(document.body);
+    injections = injections.map((injection) => {
+      const unchangedStates = injection.states.filter(
+        ({ range, cleanUp, staticRange }) => {
+          if (isEqualRange(range, staticRange)) {
+            return true;
+          }
+
+          cleanUp();
+          textIndex = getTextIndex(document.body);
+          return false;
+        }
+      );
+
+      const { nearestRanges, minDistance } = getNearestRanges(
+        textIndex,
+        injection.config.textQuoteSelector,
+        Math.min(...unchangedStates.map(({ distance }) => distance))
+      );
+
+      return {
         ...injection,
-        minDistance,
         states: [
-          ...injection.states,
-          ...nearestRanges.flatMap((range) =>
-            injection.states.some((state) => isEqualRange(state.range, range))
-              ? []
-              : [injection.config.inject(range)]
-          ),
+          ...unchangedStates,
+          ...nearestRanges.flatMap((nearestRange) => {
+            if (
+              unchangedStates.some((state) =>
+                isEqualRange(state.range, nearestRange)
+              )
+            )
+              return [];
+
+            const { range, cleanUp } = injection.config.inject(nearestRange);
+            textIndex = getTextIndex(document.body);
+
+            return [
+              {
+                range,
+                cleanUp,
+                staticRange: new StaticRange(range),
+                distance: minDistance,
+              },
+            ];
+          }),
         ],
-      }));
+      };
+    });
   } finally {
     mutationObserver.observe(document.body, {
       subtree: true,
@@ -105,7 +130,7 @@ const getNearestRanges = (
   return { nearestRanges, minDistance };
 };
 
-const isEqualRange = (a: Range, b: Range) =>
+const isEqualRange = (a: AbstractRange, b: AbstractRange) =>
   a.startContainer === b.startContainer &&
   a.startOffset === b.startOffset &&
   a.endContainer === b.endContainer &&
